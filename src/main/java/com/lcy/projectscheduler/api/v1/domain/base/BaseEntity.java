5ba38e88ac997fd6baeed76df5f0a8b3907eb0e3
lcy960729 <lcy960729@gmail.com>
2021-09-14 22:01:59 +0900

    [fix] Base 엔티티 equals, hashCode 재작성

--- src/main/java/com/lcy/projectscheduler/api/v1/domain/base/BaseEntity.java
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/base/BaseEntity.java
@@ -1,10 +1,40 @@
 package com.lcy.projectscheduler.api.v1.domain.base;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected Long id;
+
+    @Column(name = "created")
     protected LocalDateTime created;
+
+    @Column(name = "updated")
     protected LocalDateTime updated;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
