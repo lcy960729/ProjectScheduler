0dc73754184e7227ee82c057154b0ccaae2b5855
lcy960729 <lcy960729@gmail.com>
2021-09-15 18:01:04 +0900

    [add] 세션 관련 도메인 추가

--- src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/Session.java
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/Session.java
@@ -1,26 +1,58 @@
 package com.lcy.projectscheduler.api.v1.domain.project.session;
 
 import com.lcy.projectscheduler.api.v1.domain.base.BaseEntity;
+import com.lcy.projectscheduler.api.v1.domain.project.Project;
+import com.lcy.projectscheduler.api.v1.domain.project.ProjectMember;
+import com.lcy.projectscheduler.api.v1.dto.CreateSessionDTO;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+@Entity
 @NoArgsConstructor
 public class Session extends BaseEntity {
+    @Column
     private String title;
+    @Column
     private String subject;
 
+    @Column
     private LocalDateTime startDate;
+    @Column
     private LocalDateTime deadlineDate;
 
-    private List<SessionMember> sessionMembers;
+    @OneToMany(mappedBy = "session")
+    private Set<SessionMember> sessionMembers = new HashSet<>();
 
-    public Session(String title, String subject, LocalDateTime startDate, LocalDateTime deadlineDate, List<SessionMember> sessionMembers) {
+    @ManyToOne
+    @JoinColumn
+    private Project project;
+
+    private Session(String title, String subject, LocalDateTime startDate, LocalDateTime deadlineDate, Project project) {
         this.title = title;
         this.subject = subject;
         this.startDate = startDate;
         this.deadlineDate = deadlineDate;
-        this.sessionMembers = sessionMembers;
+        this.project = project;
+    }
+
+    public static Session of(Project project, CreateSessionDTO dto) {
+        return new Session(dto.getTitle(),
+                dto.getSubject(),
+                dto.getStartDate(),
+                dto.getDeadlineDate(),
+                project);
+    }
+
+    public void addMember(SessionMember sessionMember) {
+        sessionMembers.add(sessionMember);
+    }
+
+    public Set<SessionMember> getSessionMembers() {
+        return sessionMembers;
     }
 }
--- src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/SessionMember.java
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/SessionMember.java
@@ -1,10 +1,69 @@
 package com.lcy.projectscheduler.api.v1.domain.project.session;
 
+import com.lcy.projectscheduler.api.v1.domain.base.BaseEntity;
+import com.lcy.projectscheduler.api.v1.domain.member.Member;
+import com.lcy.projectscheduler.api.v1.domain.member.permission.Permission;
+import com.lcy.projectscheduler.api.v1.domain.member.permission.SessionPermission;
+import com.lcy.projectscheduler.api.v1.domain.member.state.MemberState;
 import com.lcy.projectscheduler.api.v1.domain.user.User;
+import lombok.NoArgsConstructor;
 
-public class SessionMember {
+import javax.persistence.*;
+
+
+@Entity
+@NoArgsConstructor
+public class SessionMember extends BaseEntity implements Member {
+    @ManyToOne
+    @JoinColumn
     private User user;
+
+    @ManyToOne
+    @JoinColumn
     private Session session;
 
-    private SessionAuthority sessionAuthority;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private MemberState state;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SessionPermission sessionPermission;
+
+    @Override
+    public void auth(Permission needPermission) {
+        state.checkRegistered();
+        sessionPermission.checkPermission(needPermission);
+    }
+
+    private SessionMember(User user, Session session, MemberState state, SessionPermission sessionPermission) {
+        this.user = user;
+        this.session = session;
+        this.state = state;
+        this.sessionPermission = sessionPermission;
+    }
+
+    public static SessionMember registerManager(User user, Session session) {
+        return new SessionMember(user, session, MemberState.JOINED, SessionPermission.MANAGER);
+    }
+
+    public static SessionMember registerMember(User user, Session session) {
+        return new SessionMember(user, session, MemberState.JOINED, SessionPermission.MEMBER);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public MemberState getState() {
+        return state;
+    }
+
+    public SessionPermission getSessionPermission() {
+        return sessionPermission;
+    }
 }
