a5af5c611955d39f9746f5791eafc19be49dc308
lcy960729 <lcy960729@gmail.com>
2021-09-15 18:03:51 +0900

    [fix] 프로젝트 서비스 테스트 결과에 Nested class로 변경

---
+++ src/test/java/com/lcy/projectscheduler/api/v1/domain/project/ProjectServiceTest.java
@@ -0,0 +1,70 @@
+package com.lcy.projectscheduler.api.v1.domain.project;
+
+import com.lcy.projectscheduler.api.v1.domain.member.permission.ProjectPermission;
+import com.lcy.projectscheduler.api.v1.domain.member.state.MemberState;
+import com.lcy.projectscheduler.api.v1.domain.user.User;
+import com.lcy.projectscheduler.api.v1.dto.CreateProjectDTO;
+import com.lcy.projectscheduler.api.v1.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@DisplayName("프로젝트 도메인 서비스 테스트")
+class ProjectServiceTest {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private CreateProjectDTO createProjectDTO = CreateProjectDTO.builder()
+            .title("testTitle")
+            .subject("testSubject")
+            .deadlineDate(LocalDate.now().minusDays(1))
+            .startDate(LocalDate.now().plusDays(1))
+            .build();
+
+    private User user = User.builder()
+            .email("test@test.com")
+            .pw("test")
+            .build();
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(user);
+        createProjectDTO.setManager(user.getId());
+    }
+
+    @Nested
+    @DisplayName("성공 테스트")
+    public class Success{
+        @Test
+        @DisplayName("프로젝트를 생성할때")
+        void create() {
+            // when
+            Project project = projectService.create(createProjectDTO);
+
+            // then
+            assertThat(project).isNotNull();
+
+            Set<ProjectMember> participants = project.getProjectMembers();
+
+            assertThat(participants).hasSize(1);
+
+            ProjectMember projectMember = participants.stream().findFirst().get();
+
+            assertThat(projectMember.getState()).isEqualTo(MemberState.JOINED);
+            assertThat(projectMember.getProjectPermission()).isEqualTo(ProjectPermission.SUPER_MANAGER);
+        }
+    }
+}
