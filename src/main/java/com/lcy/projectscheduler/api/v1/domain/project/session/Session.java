d266590d85783290ca4d1abe75baa9ba7caf88b3
lcy960729 <lcy960729@gmail.com>
2021-09-26 18:39:43 +0900

    [add] Session 상태 필드 추가

--- src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/Session.java
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/Session.java
@@ -2,14 +2,12 @@ package com.lcy.projectscheduler.api.v1.domain.project.session;
 
 import com.lcy.projectscheduler.api.v1.domain.base.BaseEntity;
 import com.lcy.projectscheduler.api.v1.domain.project.Project;
-import com.lcy.projectscheduler.api.v1.domain.project.ProjectMember;
 import com.lcy.projectscheduler.api.v1.dto.CreateSessionDTO;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -17,11 +15,17 @@ import java.util.Set;
 public class Session extends BaseEntity {
     @Column
     private String title;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SessionState state;
+
     @Column
     private String subject;
 
     @Column
     private LocalDateTime startDate;
+
     @Column
     private LocalDateTime deadlineDate;
 
@@ -32,8 +36,9 @@ public class Session extends BaseEntity {
     @JoinColumn
     private Project project;
 
-    private Session(String title, String subject, LocalDateTime startDate, LocalDateTime deadlineDate, Project project) {
+    private Session(String title, SessionState state, String subject, LocalDateTime startDate, LocalDateTime deadlineDate, Project project) {
         this.title = title;
+        this.state = state;
         this.subject = subject;
         this.startDate = startDate;
         this.deadlineDate = deadlineDate;
@@ -42,6 +47,7 @@ public class Session extends BaseEntity {
 
     public static Session of(Project project, CreateSessionDTO dto) {
         return new Session(dto.getTitle(),
+                SessionState.NOT_STARTED,
                 dto.getSubject(),
                 dto.getStartDate(),
                 dto.getDeadlineDate(),
@@ -54,5 +60,13 @@ public class Session extends BaseEntity {
 
     public Set<SessionMember> getSessionMembers() {
         return sessionMembers;
+    }
+
+    public boolean isNotJoined(long userId) {
+        return !sessionMembers.contains(userId);
+    }
+
+    public Project getProject() {
+        return project;
     }
 }
---
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/project/session/SessionState.java
@@ -0,0 +1,5 @@
+package com.lcy.projectscheduler.api.v1.domain.project.session;
+
+public enum SessionState {
+    NOT_STARTED, IN_PROGRESS, DONE
+}
