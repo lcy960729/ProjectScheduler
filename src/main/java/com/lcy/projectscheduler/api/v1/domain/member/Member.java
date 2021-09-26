dc3c1472be9119967e5caba284f1af2e0861172e
lcy960729 <lcy960729@gmail.com>
2021-09-26 18:43:17 +0900

    [fix] 권한 인증 필드명 변경

--- src/main/java/com/lcy/projectscheduler/api/v1/domain/member/Member.java
+++ src/main/java/com/lcy/projectscheduler/api/v1/domain/member/Member.java
@@ -3,5 +3,5 @@ package com.lcy.projectscheduler.api.v1.domain.member;
 import com.lcy.projectscheduler.api.v1.domain.member.permission.Permission;
 
 public interface Member {
-    void auth(Permission needPermission);
+    void checkRegisteredAndPermission(Permission needPermission);
 }
