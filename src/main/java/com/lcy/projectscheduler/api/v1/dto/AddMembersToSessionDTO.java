da3f2170d49962b21d369e603ba79ba5f3821502
lcy960729 <lcy960729@gmail.com>
2021-09-26 18:43:03 +0900

    [add] DTO 추가

---
+++ src/main/java/com/lcy/projectscheduler/api/v1/dto/AddMembersToSessionDTO.java
@@ -0,0 +1,20 @@
+package com.lcy.projectscheduler.api.v1.dto;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class AddMembersToSessionDTO {
+    List<Long> users = new ArrayList<>();
+
+    public List<Long> getUsers() {
+        return users;
+    }
+
+    public AddMembersToSessionDTO(long user) {
+        users.add(user);
+    }
+}
